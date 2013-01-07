e8df43eb589112557cdcf29742d2dcee03a908ae
Peter West <pete@whiteoctober.co.uk>
2013-01-07 01:04:15 +0000

    Added first unit test, just testing

---
+++ Tests/FirstTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WhiteOctober\MongoatBundle\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class SpreadsheetTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+    }
+
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function tearDown()
+    {
+    }
+}
